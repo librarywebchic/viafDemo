fc0a7e8982decfc32f40bf4edf671427cb26d3d6
Karen Coombs <librarywebchic@gmail.com>
2016-08-25 10:28:25 -0500

    Fixed influences and influnced code

--- js/controllers.js
+++ js/controllers.js
@@ -43,14 +43,63 @@ app.controller('ldcontroller', ['$scope', '$http', 'ngDialog', function($scope, 
 	    		}).then(function successCallback(sparql_response) {
 	    	  		//parse the JSON response from the SPARQL query 
 	    	  		result = angular.fromJson(sparql_response.data);
-	    	  		console.log(result);
 	    	  		$scope.birthplace = result.results.bindings[0].birthplaceLabel.value;
 	    	  		$scope.description = result.results.bindings[0].description.value;
 	    	          
 	    		}, function errorCallback(response) {
-	    	  	alert('Failed calling Wikidata');
 	    	  	console.log(response);
 	    		});
+	 
+	    	//create the sparql query to dbpedia
+	    	dbpedia_sparql_url = 'http://dbpedia.org/sparql?query=';
+	    	dbpedia_sparql_query = [
+	    	                        "SELECT ?type ?givenName ?surname",
+	    	                        "WHERE {",
+	    	                        "{",
+	    	                        "?author dbp:viaf " + viafId + " .",
+	    	                        "?s dbp:influenced ?author .",
+	    	                        "?s foaf:givenName ?givenName .",
+	    	                        "?s foaf:surname ?surname .",
+	    	                        "BIND(\"influencedBy\" AS ?type) .",
+	    	                        "}",
+	    	                        "UNION",
+	    	                        "{",
+	    	                        "?author dbp:viaf " + viafId + " .",
+	    	                        "?s dbp:influences ?author .",
+	    	                        "?s foaf:givenName ?givenName .",
+	    	                        "?s foaf:surname ?surname .",
+	    	                        "BIND(\"influences\" AS ?type) .",
+	    	                        "}",
+	    	                        "}",
+	    	                        "ORDER BY ?type",
+	    	                        ].join(" ");
+	    	dbpedia_sparql_url += dbpedia_sparql_query + '&format=json';
+	    	
+	    	$http({
+	    		method: 'GET',
+	    	  	  url: dbpedia_sparql_url
+	    		}).then(function successCallback(dbpedia_sparql_url_response) {
+	    	  		//parse the JSON response from the SPARQL query 
+	    	  		result = angular.fromJson(dbpedia_sparql_url_response.data);
+	    	  		influenced = [];
+	    	  		influences = [];
+		            nodes = result.results.bindings;
+		            for (i = 0; i < nodes.length; i++) {
+		            	if (nodes[i].type.value == 'influences') {
+		            		influenced.push(nodes[i].givenName.value + ' ' + nodes[i].surname.value);
+		            	}else{
+		            		influences.push(nodes[i].givenName.value + ' ' + nodes[i].surname.value);
+		            	}
+		            }
+		            $scope.influenced_persons = influenced;
+		            $scope.influences_persons = influences;
+		            
+		            console.log($scope);
+	    	          
+	    		}, function errorCallback(response) {
+	    	  	console.log(response);
+	    		});	    	
+	    	
 	    	
 	    	ngDialog.open({
 	    	    template: 'author-template.html',
